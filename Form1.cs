808087b83e02c7b0f3347737ad5547297dc99693
yunsungbin <dbstmdqls0211@gmail.com>
2025-05-20 09:16:28 +0900

    form추가

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using static System.Formats.Asn1.AsnWriter;
+
 namespace Eco_Quest
 {
     public partial class Form1 : Form
@@ -5,6 +7,14 @@ namespace Eco_Quest
         public Form1()
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainGame gameForm = new MainGame();
+            gameForm.Show(); //게임 화면 열기
+            this.Hide(); //현재 form 숨기기
         }
     }
 }
